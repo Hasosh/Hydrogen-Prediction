d66d5d27af1b8bfb1cc08a4f81545c95fb77d87e
Hasan Evci <zurna.hasan20@googlemail.com>
2024-06-28 19:43:07 +0200

    working recursive neighbor finder but slow and no stop of computation if unwanted atoms occur

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,4 +1,5 @@
 from gemmi import cif
+import gemmi
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -157,60 +158,171 @@ def clean_dataframe(atom_df, bond_df):
     return atom_df, bond_df
 
 
+# Function to determine the correct join column and perform the join
+def join_atom_coordinates(result_df, atom_df):
+    # Create a copy of the DataFrame to avoid modifying the original one
+    df = result_df.copy()
+
+    # Determine the correct join column
+    df['join_on_atom_id'] = df.apply(
+        lambda row: row['atom_id_2'] if row['atom_id'] == row['atom_id_1'] else row['atom_id_1'], axis=1
+    )
+
+    # Perform the join
+    merged_df = df.merge(
+        atom_df[['comp_id', 'atom_id', 'atomic_number', 'model_Cartn_x', 'model_Cartn_y', 'model_Cartn_z']],
+        left_on=['comp_id', 'join_on_atom_id'],
+        right_on=['comp_id', 'atom_id'],
+        suffixes=('', '_joined')
+    )
+
+    # Extract and rename the coordinates columns
+    merged_df = merged_df.rename(
+        columns={
+            'model_Cartn_x_joined': 'model_Cartn_x2',
+            'model_Cartn_y_joined': 'model_Cartn_y2',
+            'model_Cartn_z_joined': 'model_Cartn_z2'
+        }
+    )
+
+    # Keep only the necessary columns
+    result_df = merged_df[
+        ['comp_id', 'atom_id', 'type_symbol', 'model_Cartn_x', 'model_Cartn_y', 'model_Cartn_z', 'is_hydrogen', 
+         'bonded_hydrogens', 'atomic_number', 'atom_id_1', 'atom_id_2', 'value_order', 'model_Cartn_x2', 'model_Cartn_y2', 'model_Cartn_z2', 'atomic_number_joined']
+    ]
+
+    return result_df
+
+
+def recursive_neighbor_finder(atom_df, bond_df, current_atom, neighbor_list, current_depth, current_value_order=-1, max_depth=1):
+    if current_depth > max_depth:
+        return
+
+    comp_id, atom_id = current_atom
+
+    # Find the current atom in atom_df and save its coordinates along with comp_id and atom_id
+    atom_info = atom_df[(atom_df['comp_id'] == comp_id) & (atom_df['atom_id'] == atom_id)]
+    assert len(atom_info) == 1, "atom not found"
+
+    # Add infos of the current atom to the neighbor list
+    if not atom_info.empty:
+        current_atomic_number = atom_info.iloc[0]['atomic_number']
+        x, y, z = atom_info.iloc[0]['model_Cartn_x'], atom_info.iloc[0]['model_Cartn_y'], atom_info.iloc[0]['model_Cartn_z']
+        if current_depth not in neighbor_list:
+            neighbor_list[current_depth] = []
+        neighbor_list[current_depth].append((comp_id, atom_id, current_atomic_number, current_value_order, x, y, z))
+
+    # Find all bonds in bond_df involving the current atom
+    bonds_involving_atom = bond_df[(bond_df['comp_id'] == comp_id) & 
+                                   ((bond_df['atom_id_1'] == atom_id) | (bond_df['atom_id_2'] == atom_id))]
+
+    # For each bond, determine the neighboring atom and recursively call the method
+    for _, bond in bonds_involving_atom.iterrows():
+        bond_value_order = bond['value_order']
+        if bond['atom_id_1'] == atom_id:
+            neighbor_atom = (bond['comp_id'], bond['atom_id_2'])
+        else:
+            neighbor_atom = (bond['comp_id'], bond['atom_id_1'])
+
+        # Check if the neighbor_atom has already been handled
+        already_handled = any(neighbor_atom == (comp_id, atom_id) for depth, neighbors in neighbor_list.items() for comp_id, atom_id, _, _, _, _, _ in neighbors)
+        if not already_handled:
+            # Recursively call the function for the neighboring atom
+            recursive_neighbor_finder(atom_df, bond_df, neighbor_atom, neighbor_list, current_depth + 1, bond_value_order, max_depth)
+
+
+
+
 if __name__ == "__main__":
-    doc = cif.read_file("../data/components.cif")
-    print('Numver of compounds in dataset: '+ str(len(doc)))
+    if not Config.dev:
+        doc = cif.read_file("../data/components.cif")
+        print('Number of blocks/molecules in dataset: '+ str(len(doc)))
 
-    if Config.make_bond_df:
-        print("Making bond_df")
-        create_bond_df(doc)
-        print("Finished making bond_df")
-    
-    if Config.make_atom_df:
-        print("Making atom_df")
-        create_atom_df(doc)
-        print("Finished making atom_df")
+        if Config.make_bond_df:
+            print("Making bond_df")
+            create_bond_df(doc)
+            print("Finished making bond_df")
+        
+        if Config.make_atom_df:
+            print("Making atom_df")
+            create_atom_df(doc)
+            print("Finished making atom_df")
 
-    if Config.make_atom_df_extended:
-        try:
-            atom_df = pd.read_csv('../data/atom_df.csv')
-            bond_df = pd.read_csv('../data/bond_df.csv')
-        except FileNotFoundError as e:
-            print(f"File not found: {e.filename}")
-        except pd.errors.EmptyDataError as e:
-            print(f"No data: {e}")
-        except pd.errors.ParserError as e:
-            print(f"Parsing error: {e}")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
+        if Config.make_atom_df_extended:
+            try:
+                atom_df = pd.read_csv('../data/atom_df.csv')
+                bond_df = pd.read_csv('../data/bond_df.csv')
+            except FileNotFoundError as e:
+                print(f"File not found: {e.filename}")
+            except pd.errors.EmptyDataError as e:
+                print(f"No data: {e}")
+            except pd.errors.ParserError as e:
+                print(f"Parsing error: {e}")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
 
-        print("Making atom_df_extended")
-        create_atom_df_extended(atom_df, bond_df)
-        print("Finished making atom_df_extended")
+            print("Making atom_df_extended")
+            create_atom_df_extended(atom_df, bond_df)
+            print("Finished making atom_df_extended")
 
-    # load atom_df and bond_df
-    atom_df = pd.read_csv('../data/atom_df_extended.csv')
-    bond_df = pd.read_csv('../data/bond_df.csv')
+        # load atom_df and bond_df
+        atom_df = pd.read_csv('../data/atom_df_extended.csv')
+        bond_df = pd.read_csv('../data/bond_df.csv')
 
-    # Before cleaning nan and ? rows, check that we did not impute these values by ourselves
-    assert atom_df['is_hydrogen'].notna().all(), f"NaN values found in column 'is_hydrogen'"
-    assert atom_df['bonded_hydrogens'].notna().all(), f"NaN values found in column 'bonded_hydrogens'"
+        # Before cleaning nan and ? rows, check that we did not impute these values by ourselves
+        assert atom_df['is_hydrogen'].notna().all(), f"NaN values found in column 'is_hydrogen'"
+        assert atom_df['bonded_hydrogens'].notna().all(), f"NaN values found in column 'bonded_hydrogens'"
 
-    # clean dataframe
-    atom_df, bond_df = clean_dataframe(atom_df, bond_df)
+        # clean dataframe
+        atom_df, bond_df = clean_dataframe(atom_df, bond_df)
 
-    ### Modify columns
-    # make new column atomic number and depending on the type symbol used, give the atomic number
-    # E.g. in the periodic table, the atom C has the atomic number 6
-    atom_df['atomic_number'] = atom_df['type_symbol'].apply(lambda x: gemmi.Element(x).atomic_number)
+        ### Modify columns
+        # make new column atomic number and depending on the type symbol used, give the atomic number
+        # E.g. in the periodic table, the atom C has the atomic number 6
+        atom_df['atomic_number'] = atom_df['type_symbol'].apply(lambda x: gemmi.Element(x).atomic_number)
 
 
-    # Define the mapping from string to integer
-    value_order_mapping = {
-        'SING': 1,
-        'DOUB': 2,
-        'TRIP': 3
-    }
+        # Define the mapping from string to integer
+        value_order_mapping = {
+            'SING': 1,
+            'DOUB': 2,
+            'TRIP': 3
+        }
 
-    # Replace the values in the 'value_order' column
-    bond_df['value_order'] = bond_df['value_order'].replace(value_order_mapping)
+        # Replace the values in the 'value_order' column
+        bond_df['value_order'] = bond_df['value_order'].replace(value_order_mapping)
+
+        # Save the updated atom_df to CSV
+        atom_df.to_csv('../data/atom_df_extended_filtered.csv', index=False)
+        bond_df.to_csv('../data/bond_df_filtered.csv', index=False)
+    else:
+        # load atom_df and bond_df
+        atom_df = pd.read_csv('../data/atom_df_extended_filtered.csv')
+        bond_df = pd.read_csv('../data/bond_df_filtered.csv')
+
+        ### Training / Testing dataset generation
+        atom_df_filtered = atom_df.loc[(atom_df['type_symbol'] == Config.central_atom) & (atom_df['bonded_hydrogens'] == Config.num_hydrogens)]
+
+        neighbor_list = {}
+        current_atom = ('001', 'C02')  # Example atom
+        recursive_neighbor_finder(atom_df, bond_df, current_atom, neighbor_list, current_depth=0, current_value_order=-1, max_depth=Config.max_neighbor_depth)
+        print(neighbor_list)
+
+        # # Merge on comp_id and atom_id_1
+        # merged_1 = atom_df_filtered.merge(bond_df, left_on=['comp_id', 'atom_id'], right_on=['comp_id', 'atom_id_1'], suffixes=('', '_bond_1'))
+
+        # # Merge on comp_id and atom_id_2
+        # merged_2 = atom_df_filtered.merge(bond_df, left_on=['comp_id', 'atom_id'], right_on=['comp_id', 'atom_id_2'], suffixes=('', '_bond_2'))
+
+        # # Concatenate the two merged DataFrames
+        # result_df = pd.concat([merged_1, merged_2], ignore_index=True)
+        # print(result_df.shape[0])
+
+        # # Apply the function and print the result
+        # result_df_final = join_atom_coordinates(result_df, atom_df)
+
+        # # Sort the result_df by comp_id first and then by atom_id
+        # result_df_final.sort_values(by=['comp_id', 'atom_id'], inplace=False).reset_index(drop=True)
+
+        # #print(result_df_final.head(15))
+        # print(result_df_final.shape[0])
